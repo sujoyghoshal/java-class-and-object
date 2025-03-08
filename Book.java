4585d92ffc219ef9c3f28fc7f497d893531107f7
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-08 14:09:50 +0530

    [sujoy]desc: Program to Handle Book Details

---
+++ Book.java
@@ -0,0 +1,24 @@
+class Book {
+    String title;
+    String author;
+    double price;
+
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "James Gosling", 499.99);
+        book.displayDetails();
+    }
+}
+// Input: Book details
+// Output: Displays Book details
